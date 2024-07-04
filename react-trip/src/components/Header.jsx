89c0a90b617f0a6060752c2181f1227b15c3c7fd
pricbnll <pricbnll@gmail.com>
2024-07-04 19:33:03 -0300

    adicionado exercicio da aula 6

---
+++ react-trip/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <>
+        <div className="header">
+            <img src="https://www.origamid.com/projetos/bikcraft/img/bikcraft.svg" alt="Logo Bikcraft" />
+            <ul>
+                <li>Bicicletas</li>
+                <li>Seguros</li>
+                <li>Contato</li>
+            </ul>
+        </div>
+        {/* <h1>Header</h1> */}
+        </>
+    )
+}
+
+export default Header
